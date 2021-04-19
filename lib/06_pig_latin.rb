9079b1b5847d056d5662d0f15c0a30b5ccba05a9
Xabi <xabi.aycaguer@gmail.com>
2021-04-19 22:24:33 +0200

    test ruby V Final

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,18 +1,18 @@
 def translate(str)
   vowels="aeiou".chars
   array_str=str.split.map do |word|
-    if word[0]=="a" || word[0]=="e" || word[0]=="i" || word[0]=="o" || word[0]=="u"
+    if vowels.include?(word[0])==true
       word+"ay"
 
     elsif word.include?("qu")
       if word[0]!=vowels && word[0]!="q"
-        word[3..word.size]+word[0]+"qu"+"ay"
+        word[3..word.size]+word[0]+"quay"
         else
-        word.delete("qu")+"qu"+"ay"
+        word.delete("qu")+"quay"
       end
 
     elsif word.include?("sch")
-      word.delete("sch")+"sch"+"ay"
+      word.delete("sch")+"schay"
 
     elsif vowels.include?(word[0])==false && vowels.include?(word[1])==false && vowels.include?(word[2])==false
       word[3..word.size]+word[0..2]+"ay"
